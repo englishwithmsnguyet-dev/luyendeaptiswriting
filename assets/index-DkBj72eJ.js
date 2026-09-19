013b3654370697b2ccee47089fb347e53d307b55
englishwithmsnguyet-dev <englishwithmissnguyet@gmail.com>
2026-09-20 05:43:51 +0700

    Updates

--- assets/index-DkBj72eJ.js
+++ assets/index-DkBj72eJ.js
@@ -4,5 +4,6 @@ if (!sessionStorage.getItem('aptis_cache_refresh')) {
   sessionStorage.setItem('aptis_cache_refresh', 'true');
   window.location.reload(true);
 } else {
+  // If already tried reload, redirect to root hash route
   window.location.href = window.location.pathname.replace(/\/assets\/.*$/, '/') + '#/';
 }
